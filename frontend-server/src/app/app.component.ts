ee83bb813cb95e7d59937ba9229f31730457c072
Burgas <burgassme@gmail.com>
2025-03-17 16:38:40 +0700

    Adding component cards

--- frontend-server/src/app/app.component.ts
+++ frontend-server/src/app/app.component.ts
@@ -1,6 +1,5 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {IdentityCardComponent} from './cards/identity-card/identity-card.component';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
