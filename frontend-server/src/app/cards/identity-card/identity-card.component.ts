39da88456778e546c5bfaf045dbb53dd245847c9
Burgas <burgassme@gmail.com>
2025-03-17 16:30:08 +0700

    Adding frontend-server module

---
+++ frontend-server/src/app/cards/identity-card/identity-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-identity-card',
+  imports: [],
+  templateUrl: './identity-card.component.html',
+  styleUrl: './identity-card.component.css'
+})
+export class IdentityCardComponent {
+
+}
