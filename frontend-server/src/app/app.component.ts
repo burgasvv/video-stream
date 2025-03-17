d6d91e96071ed6f3b06e607f8c3fa92e02b5ac3e
Burgas <burgassme@gmail.com>
2025-03-17 16:38:16 +0700

    Adding component cards

--- frontend-server/src/app/app.component.ts
+++ frontend-server/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {IdentityCardComponent} from './cards/identity-card/identity-card.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, IdentityCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ frontend-server/src/app/cards/streamer-card/streamer-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-streamer-card',
+  imports: [],
+  templateUrl: './streamer-card.component.html',
+  styleUrl: './streamer-card.component.css'
+})
+export class StreamerCardComponent {
+
+}
