4a7c3e5132252afbc8aba3389c61c800b81f9281
Burgas <burgassme@gmail.com>
2025-03-22 23:52:05 +0700

    Add stream entity and service

--- frontend-server/src/app/layout/layout.component.ts
+++ frontend-server/src/app/layout/layout.component.ts
@@ -1,14 +1,12 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {HeaderComponent} from '../components/header/header.component';
 import {RouterOutlet} from '@angular/router';
-import {FooterComponent} from '../components/footer/footer.component';
 
 @Component({
   selector: 'app-layout',
     imports: [
         HeaderComponent,
-        RouterOutlet,
-        FooterComponent
+        RouterOutlet
     ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
