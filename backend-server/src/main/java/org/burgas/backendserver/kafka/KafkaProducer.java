ae3ddeee6b364d98126dd109dc10be3cf148b0ce
Burgas <burgassme@gmail.com>
2025-04-03 19:30:07 +0700

    Adding kafka for sending and answering on invitations

--- backend-server/src/main/java/org/burgas/backendserver/kafka/KafkaProducer.java
+++ backend-server/src/main/java/org/burgas/backendserver/kafka/KafkaProducer.java
@@ -1,6 +1,5 @@
 package org.burgas.backendserver.kafka;
 
-import org.burgas.backendserver.dto.InvitationAnswer;
 import org.burgas.backendserver.dto.InvitationResponse;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
